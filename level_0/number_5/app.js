e826300162a421b01a12520f6f5f618d8191c776
banma1234 <banma1234@gmail.com>
2023-01-14 16:26:09 +0900

    feat(level_1) 1 : 없는 숫자 더하기 solved

--- level_0/number_5/app.js
+++ level_0/number_5/app.js
@@ -1,18 +1,12 @@
 function solution(numer1, denom1, numer2, denom2) {
-    const getGCD = (num1, num2) => (num2 > 0 ? getGCD(num2, num1%num2) : num1);
-
-    const GCD = getGCD(denom1, denom2);
-    const LCM = denom1 * denom2 / GCD;
-
-    let x = (LCM/denom1*numer1)+(LCM/denom2*numer2);
-    const GCD_2 = getGCD(x, LCM);
-
-    return answer = [x/GCD_2, LCM/GCD_2];
+    let answer = 0;
+    numbers.forEach((e) => {
+        answer += e
+    })
+    
+    return 45 - answer;
 }
 
-let a = 9;
-let b = 1;
-let denom1 = 2;
-let denom2 = 3;
+let numbers = [1,2,3,4,6,7,8,0];
 
-console.log(solution(a, denom1, b, denom2));
+console.log(solution(numbers));
