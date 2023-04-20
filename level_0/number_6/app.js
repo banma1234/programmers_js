237a46bbc2ad0e316a6c6cd6eda5726048e00450
banma1234 <banma1234@gmail.com>
2023-04-20 18:31:13 +0900

    design: prettier format 적용

--- level_0/number_6/app.js
+++ level_0/number_6/app.js
@@ -1,28 +1,30 @@
 function solution(quiz) {
-    var answer = [];
-    let temp = [];
+  var answer = [];
+  let temp = [];
 
-    quiz.forEach(item => {
-        temp = item.split(' ');
-        switch(temp[1]) {
-            case '+':
-                if (parseInt(temp[0]) + parseInt(temp[2]) == parseInt(temp[4])) answer.push('O');
-                else    answer.push('X');
-                break;
-            case '-':
-                if (parseInt(temp[0]) - parseInt(temp[2]) == parseInt(temp[4])) answer.push('O');
-                else    answer.push('X');
-                break;
-        }
-    });
-    return answer;
+  quiz.forEach(item => {
+    temp = item.split(" ");
+    switch (temp[1]) {
+      case "+":
+        if (parseInt(temp[0]) + parseInt(temp[2]) == parseInt(temp[4]))
+          answer.push("O");
+        else answer.push("X");
+        break;
+      case "-":
+        if (parseInt(temp[0]) - parseInt(temp[2]) == parseInt(temp[4]))
+          answer.push("O");
+        else answer.push("X");
+        break;
+    }
+  });
+  return answer;
 }
 
 // Array의 메소드에대한 이해를 확실히 다지자
 function bestSolution(quiz) {
   return quiz
-    .map((el) => el.split(" = "))
-    .map((el) => {
+    .map(el => el.split(" = "))
+    .map(el => {
       return eval(el[0]) == el[1] ? "O" : "X";
     });
 }
