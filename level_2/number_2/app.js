37143b060bc451f99b72236943c2d36514926c19
banma1234 <banma1234@gmail.com>
2023-06-03 17:57:06 +0900

    refactor: 불필요 구문 제거

--- level_2/number_2/app.js
+++ level_2/number_2/app.js
@@ -66,21 +66,3 @@ function solution(fees, records) {
 
   return answer;
 }
-
-const fees = [180, 5000, 10, 600];
-const records = [
-  "05:34 5961 IN",
-  "06:00 0000 IN",
-  "06:34 0000 OUT",
-  "07:59 5961 OUT",
-  "07:59 0148 IN",
-  "18:59 0000 IN",
-  "19:09 0148 OUT",
-  "22:59 5961 IN",
-  "23:00 5961 OUT",
-];
-
-const fees2 = [1, 461, 1, 10];
-const records2 = ["00:00 1234 IN"];
-
-console.log(solution(fees, records));
