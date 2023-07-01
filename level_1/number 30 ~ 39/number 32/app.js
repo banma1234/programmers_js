8fcd34acadbd4e0371ce6f4c3e799b99186c9584
banma1234 <banma1234@gmail.com>
2023-07-01 17:08:26 +0900

    fix: 기존 solution + 개선된 solution 추가 작성

--- level_1/number 30 ~ 39/number 32/app.js
+++ level_1/number 30 ~ 39/number 32/app.js
@@ -1,4 +1,5 @@
-function solution(X, Y) {
+// 개선된 solution
+function betterSolution(X, Y) {
   const mapOfX = {};
   let result = "";
 
@@ -18,4 +19,34 @@ function solution(X, Y) {
     .map(e => Number(e))
     .sort((a, b) => b - a)
     .join("");
+}
+
+// 최초로 작성한 solution
+function solution(X, Y) {
+  let mapOfX = {};
+  let mapOfY = {};
+  let result = [];
+
+  X.split("").forEach(item => {
+    mapOfX[item] ? (mapOfX[item] += 1) : (mapOfX[item] = 1);
+  });
+  Y.split("").forEach(item => {
+    mapOfY[item] ? (mapOfY[item] += 1) : (mapOfY[item] = 1);
+  });
+
+  Object.keys(mapOfY).forEach(item => {
+    if (mapOfX[item]) {
+      let min = Math.min(mapOfX[item], mapOfY[item]);
+      for (let i = 0; i < min; i++) {
+        result.push(item);
+      }
+    }
+  });
+
+  if (result.length === 0) {
+    return "-1";
+  } else {
+    result = result.sort((a, b) => b - a).join("");
+    return Number(result) > 0 ? result : "0";
+  }
 }
