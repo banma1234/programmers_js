1b4d5886257e7d797904c837ea2ca0108d5094f3
banma1234 <banma1234@gmail.com>
2023-09-09 15:52:16 +0900

    feat(level_1) 36 : solved

---
+++ level_1/number 30 ~ 39/number_36/app.js
@@ -0,0 +1,16 @@
+function solution(lottos, win_nums) {
+  let min = 0;
+  const picked = lottos.filter(item => item != 0);
+  const win_nums_map = {};
+
+  win_nums.forEach(item => (win_nums_map[item] = true));
+  picked.forEach(item => {
+    if (win_nums_map.hasOwnProperty(item)) {
+      min++;
+      delete win_nums_map[item];
+    }
+  });
+  let max = min + (6 - picked.length);
+
+  return [max ? 7 - max : 6, min ? 7 - min : 6];
+}
