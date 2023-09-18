e142fddbd7ff7a6e128ecc29cf455a58b4ce562c
banma1234 <banma1234@gmail.com>
2023-09-18 17:05:56 +0900

    feat(level_1) 39 : solved

---
+++ level_1/number 30 ~ 39/number_39/app.js
@@ -0,0 +1,29 @@
+function solution(N, stages) {
+  const map = {};
+  const failed = [];
+  let result = [];
+  let countOfStage = stages.length;
+
+  for (let i = 1; i < N + 1; i++) {
+    map[i] = 0;
+  }
+
+  stages.forEach(item => {
+    if (map[item] != undefined) {
+      map[item]++;
+    }
+  });
+
+  const value = Object.values(map);
+  value.forEach(item => {
+    failed.push(item / countOfStage);
+    countOfStage -= item;
+  });
+
+  result = failed
+    .map((failed, i) => ({ failed, i }))
+    .sort((a, b) => b.failed - a.failed)
+    .map(({ i }) => i + 1);
+
+  return result;
+}
