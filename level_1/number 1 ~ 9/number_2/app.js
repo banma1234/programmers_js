d51ef8225f642ed6968e01cc9b5be6d29408534d
banma1234 <banma1234@gmail.com>
2024-03-20 15:20:12 +0900

    chore: prettier format 적용

--- level_1/number 1 ~ 9/number_2/app.js
+++ level_1/number 1 ~ 9/number_2/app.js
@@ -1,14 +1,13 @@
 function solution(sizes) {
-    let maxW = 0;
-    let maxH = 0;
+  let maxW = 0;
+  let maxH = 0;
 
-    sizes.forEach((item) => {
-        item.sort((x, y) => { return y - x });
-        if(maxW <= item[0]) maxW = item[0];
-        if(maxH <= item[1]) maxH = item[1];
-    })
-    return maxW * maxH;
+  sizes.forEach(item => {
+    item.sort((x, y) => {
+      return y - x;
+    });
+    if (maxW <= item[0]) maxW = item[0];
+    if (maxH <= item[1]) maxH = item[1];
+  });
+  return maxW * maxH;
 }
-
-const sizes = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]];
-console.log(solution(sizes));
